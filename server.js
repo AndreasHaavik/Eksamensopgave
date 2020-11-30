8d2661998bc50e65b46f02cc0af734cc2507dcec
Andreas Haavik <andreashaavik@Andreass-MacBook-Pro-2.local>
2020-11-30 13:43:52 +0100

    samlet min api og server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require('http');
-const API = require('./API')
-
+const express  = require('express');
+const API = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(API);
-
 API.listen(PORT);
+const usersRoutes = require('./routes/users');
+
+ 
+API.use('/users', usersRoutes);
 
 console.log("I got my server to run, and thats amaziiin!")
