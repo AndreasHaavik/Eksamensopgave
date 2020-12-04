3092b28f3141eae4ead5d6238ba56ff45a1404eb
Andreas Haavik <andreashaavik@Andreass-MacBook-Pro-2.local>
2020-12-04 18:15:27 +0100

    commiter fra idags arbejde

--- routes/users.js
+++ routes/users.js
@@ -26,11 +26,33 @@ const User = require('../Models/User');
 // det her er når man skal have alle mine users frem
 router.get('/', (req, res, next)=>{
     User.find()
+    .select('firstName lastName gender age interests address eMail userName passWord _id')
     .exec()
     .then(docs => {
-        console.log(docs);
+       const response = {
+           count: docs.length,
+           users: docs.map(doc => {
+               return {
+                   firstName: doc.firstName,
+                   lastName: doc.lastName,
+                   gender: doc.gender,
+                   age: doc.age,
+                   interests: doc.intersts,
+                   address: doc.address,
+                   eMail: doc.eMail,
+                   userName: doc.userName,
+                   passWord: doc.passWord,
+                   _id: doc._id,
+                   request: {
+                       type: 'GET',
+                       description: 'GET YOUR USER',
+                       url: 'http://localhost:3001/users/' + doc._id
+                   }
+               }
+           })
+       };
     //      if (docs.length >= 0 ){
-            res.status(200).json(docs);
+            res.status(200).json(response);
     //       }else {
     //        res.status(404).json({
     //        message: 'no users found'
@@ -63,12 +85,28 @@ router.post('/', (req, res, next)=>{
         passWord: req.body.passWord
     });
     user
-    .save()
-    .then(result => {
-        console.log(result)
+        .save()
+        .then(result => {
+         console.log(result)
         res.status(201).json({
-            message: 'handling Post request to /users',
-            createdUser: result
+            message: 'We have now created a new user successfully!',
+            createdUser: {
+                firstName: result.firstName,
+                lastName: result.lastName,
+                gender: result.gender,
+                age: result.age,
+                interests: result.intersts,
+                address: result.address,
+                eMail: result.eMail,
+                userName: result.userName,
+                passWord: result.passWord,
+                _id: result._id,  
+                request: {
+                    type: 'GET',
+                    description: 'DIRECT LINK TO YOUR USER',
+                    url: 'http://localhost:3001/users/' + result._id
+                }
+            }
         });
     })
     .catch(err => {
@@ -90,11 +128,19 @@ router.post('/', (req, res, next)=>{
 router.get('/:userId',(req, res, next)=>{
     const id = req.params.userId;
     User.findById(id)
+    .select('firstName lastName gender age interests address eMail userName passWord _id')
     .exec()
     .then(doc => {
         console.log("from database", doc);
         if(doc) {
-            res.status(200).json(doc)
+            res.status(200).json({
+                user: doc,
+                request: {
+                    type: 'GET',
+                    description: 'DIRECT LINK TO ALL THE USERS',
+                    url: 'http://localhost:3001/users/' 
+                }
+            })
         } else {
             res.status(404).json({
                 message: 'no valid id number register'
@@ -116,8 +162,14 @@ router.patch('/:userId',(req, res, next)=>{
     User.update({_id: id}, { $set: updateOps})
     .exec()
     .then(result => {
-        console.log(result);
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'User is updated',
+            request: {
+                type: 'GET',
+                description: 'DIRECT LINK TO ALL THE USERS',
+                url: 'http://localhost:3001/users/' + id
+            }
+        })
     })
     .catch(err => {
         console.log(err)
@@ -133,7 +185,15 @@ router.delete ('/:userId',(req, res, next)=>{
     User.remove({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'User is deleted',
+                request: {
+                    type: 'POST',
+                    description: 'CLICK THE LINK TO MAKE A NEW USER',
+                    url: 'http://localhost:3001/users/',
+                    newUser: {firstName: "String", lastname: "String", gender: "String", age: "Number", interests: "String", address: "String and Number" , eMail: "String and Number", userName: "String and Number", passWord: "String and Number"}
+                }
+            });
         })
         .catch(err => { 
             console.log(err);
